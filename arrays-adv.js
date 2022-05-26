2131d90dd81c62361b259154370e079bd55d0c26
Frae <efvaldehuesa@gmail.com>
2022-05-26 16:59:22 +0800

    Initial commit

---
+++ arrays-adv.js
@@ -0,0 +1,3 @@
+// 1. Create two functions: findSmallest and findLargest where these functions will take an array of numbers as a parameter. The findSmallest function returns the smallest number from the array while the findLargest returns the largest number from the array.
+
+// 2. Create a function printEven that takes an array of numbers and will return all even numbers from the array.
---
+++ arrays.js
@@ -0,0 +1,26 @@
+/*
+Assume you are starting with the following people array:
+
+let people = ["Greg", "Mary", "Devon", "James"];
+
+*/
+
+// 1. Using a loop, iterate through this array and console.log all of the people
+
+// 2. Write the command to remove "Greg" from the array.
+
+// 3. Write the command to remove "James" from the array.
+
+// 4. Write the command to add "Matt" to the front of the array.
+
+// 5. Write the command to add your name to the end of the array.
+
+// 6. Using a loop, iterate through this array and after console.log-ing "Mary", exit the loop.
+
+// 7. Write the command to make a copy of the array - the copy should not include "Mary" or "Matt".
+
+// 8. Write the command that gives the index where "Mary" is located.
+
+// 9. Write the command that gives the index where "Foo" is located (should return -1)
+
+// 10. Redefine the people variable with the value you started with. Using the splice command, remove "Devon" from the array and add "Elizabeth" and "Artie". Your array should look like this when you are done: ["Greg", "Mary", "Elizabeth", "Artie", "James"]
